a622dd98cab91ebe44990932722746cda4f6ee05
lenters <lenters@aemics.nl>
2021-09-08 16:03:12 +0200

    PYggi: Neopixel library added.

--- ports/stm32/boards/AEMICS_PYGGI/manifest.py
+++ ports/stm32/boards/AEMICS_PYGGI/manifest.py
@@ -1,4 +1,5 @@
 freeze("$(BOARD_DIR)", ("pyg.py"))
 freeze("$(BOARD_DIR)", ("rfcore_firmware.py", "ble_repl.py"))
+freeze("$(BOARD_DIR)", ("neopixel.py"))
 
 include("$(PORT_DIR)/boards/manifest.py")
